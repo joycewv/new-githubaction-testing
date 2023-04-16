da8e9a6ecfba7709d243c1fb5746e62555e0bcd5
joycewv <114795700+joycewv@users.noreply.github.com>
2023-04-16 16:42:52 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Just a test on Build and push Docker images by using github action.</h1>
+    </div>
+  );
+}
+
+export default App;
